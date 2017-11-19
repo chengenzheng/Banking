bc90a3e5562ed4df05631eda1165322f485b2861
chengenzheng <32253758+chengenzheng@users.noreply.github.com>
2017-11-19 01:54:50 -0600

    assignment 9

---
+++ Banking/TransactionTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TransactionTableViewController.swift
+//  Banking
+//
+//  Created by chengen Zheng on 2017/11/14.
+//  Copyright © 2017年 chengen Zheng. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    
+    
+    
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Banking' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Banking
+
+ pod ‘Firebase/Core’
+ pod ‘ChameleonFramework/Swift’
+  pod 'Firebase/Auth’
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+  pod 'Firebase/Messaging'
+
+
+end
